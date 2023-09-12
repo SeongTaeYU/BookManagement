f3d6ad7c1ef0fae2d8d761eeb81eb3d0842237e1
PC <sungtaeyou@gmail.com>
2023-09-12 22:19:24 +0900

    Publisher Entity

---
+++ src/main/java/com/book/entity/Publisher.java
@@ -0,0 +1,69 @@
+package com.book.entity;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.EnumType;
+import javax.persistence.Enumerated;
+import javax.persistence.GeneratedValue;
+import javax.persistence.GenerationType;
+import javax.persistence.Id;
+import lombok.AccessLevel;
+import lombok.Builder;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.NonNull;
+import lombok.ToString;
+import org.hibernate.annotations.Where;
+
+/**
+ * @CreateBy: SeongTae
+ * @Date: 2023/09/13
+ */
+
+@Where(clause = "publisherStatus = REGISTER")
+@ToString
+@NoArgsConstructor (access = AccessLevel.PROTECTED)
+@Getter
+@Entity
+public class Publisher extends BaseTimeEntity {
+
+    @Id
+    @GeneratedValue (strategy = GenerationType.IDENTITY)
+    private Long publisherId;
+
+    @NonNull
+    @Column (nullable = false)
+    private String businessNumber;
+
+    @NonNull
+    @Column (nullable = false)
+    private String telNumber;               // 대표 전화번호
+
+    @NonNull
+    @Column (nullable = false)
+    private String name;
+
+    @NonNull
+    @Column (nullable = false)
+    private String address;
+
+    @NonNull
+    @Column (nullable = false)
+    @Enumerated (value = EnumType.STRING)
+    private PublisherStatus publisherStatus;
+
+    @Builder
+    public Publisher ( @NonNull String businessNumber, @NonNull String telNumber,
+        @NonNull String name, @NonNull String address,
+        @NonNull PublisherStatus publisherStatus ) {
+        this.businessNumber = businessNumber;
+        this.telNumber = telNumber;
+        this.name = name;
+        this.address = address;
+        this.publisherStatus = publisherStatus;
+    }
+
+    public void terminate() {
+        this.publisherStatus = PublisherStatus.UNREGISTER;
+    }
+}
