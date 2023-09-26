83b5361130c7afc08781824e2a4a5f50a302ccfb
PC <sungtaeyou@gmail.com>
2023-09-26 23:49:54 +0900

    ApiBookController 업데이트

--- src/main/java/com/book/controller/ApiBookController.java
+++ src/main/java/com/book/controller/ApiBookController.java
@@ -5,12 +5,15 @@ import java.net.URI;
 import javax.validation.Valid;
 
 import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.book.dto.request.BookSaveRequest;
+import com.book.dto.response.BookDetailResponse;
 import com.book.service.ApiBookService;
 
 import lombok.RequiredArgsConstructor;
@@ -37,5 +40,11 @@ public class ApiBookController {
 		bookService.saveNewBook(bookSaveRequest);
 		return ResponseEntity.created(URI.create("/api/books")).build();
 	}//end saveNewBook
+	
+	@GetMapping("/{id}")
+	public ResponseEntity<BookDetailResponse> findBookById (@PathVariable Long id) {
+		log.info("Book Id: {} ", id);
+		return ResponseEntity.ok().body(bookService.findBookDetailById(id));
+	}
 	
 }//end Class Controller
